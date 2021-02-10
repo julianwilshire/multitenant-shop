e14bba9f9356befe57abbd0f55bf0f3a7bfbcd96
Wladimir Avila <wladimiravila@apdi.co>
2021-02-10 03:08:10 -0500

    initial commit with tenants v1

---
+++ saleor/saleor/tenants/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django_tenants.models import TenantMixin, DomainMixin
+
+class Tenant(TenantMixin):
+    name = models.CharField(max_length=100)
+    created_on = models.DateField(auto_now_add=True)
+    def __str__(self):
+        return self.name
+
+
+class Domain(DomainMixin):
+    pass
